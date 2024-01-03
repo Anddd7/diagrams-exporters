22006a3b1297b3d35714e6c86fd880ba8554e353
Anddd7 <anddd7@icloud.com>
2024-01-03 12:11:58 +0800

    🚀feature: keep id in cache, for edge usage

--- diagrams_exporters/__init__.py
+++ diagrams_exporters/__init__.py
@@ -7,7 +7,7 @@ from diagrams_patterns import (
 )
 from diagrams_patterns.aws import VPCCluster
 
-DEBUG = False
+DEBUG = True
 
 
 def debug(msg):
@@ -21,84 +21,91 @@ class TerraformGraph:
 
 
 class Resource:
-    def __init__(self, keyword: str, type: str, name: str):
-        self.keyword = keyword
+    def __init__(self, id: str, type: str, resource_type: str, resource_name: str):
+        self.id = id
         self.type = type
-        self.name = name
+        self.resource_type = resource_type
+        self.resource_name = resource_name
         self.resources = {}
 
     def add_resource(self, resource: "Resource"):
-        self.resources[resource.get_id()] = resource
+        self.resources[resource.get_name()] = resource
         resource.parent = self
 
-    def get_id(self):
-        if self.name == "":
-            return self.type
-        return f"{self.type}.{self.name}"
+    def get_name(self):
+        if self.resource_name == "":
+            return self.resource_type
+        return f"{self.resource_type}.{self.resource_name}"
 
 
 def parse_dot(filename: str):
     graph = pydot.graph_from_dot_file(filename)[0]
     graph = graph.get_subgraphs()[0]
 
-    root = Resource("root", "root", "root")
+    root = Resource("root", "root", "root", "root")
     for node in graph.get_nodes():
-        names = node.get_label().strip('"').split(".")
         parent = root
+        label = node.get_label().strip('"')
+        names = label.split(".")
         while len(names) > 0:
-            if names[0] == "data":
-                keyword = "data"
-                # data.aws_arn.assumerole
-                type = f"{names[0]}.{names[1]}"
-                name = names[2]
+            if label.startswith("provider"):
+                type = "provider"
+                if names[-1].endswith("]"):
+                    resource_type = label
+                    resource_name = ""
+                else:
+                    resource_type = ".".join(names[:-1])
+                    resource_name = names[-1]
+
+                names = []
+            elif names[0] == "data":
+                type = "data"
+                resource_type = f"{names[0]}.{names[1]}"
+                resource_name = names[2]
 
                 names = names[3:]
-            elif names[0].startswith("provider"):
-                # provider[\"registry.terraform.io/hashicorp/aws\"]
-                keyword = "provider"
-                if names[-1].endswith("]"):
-                    type = node.get_label()
-                    name = ""
-                else:
-                    type = ".".join(names[:-1])
-                    name = names[-1]
-                break
             else:
-                keyword = names[0]
-                type = names[0]
-                name = names[1]
+                type = names[0]  # include module & var
+                resource_type = names[0]
+                if len(names) == 1:
+                    debug(f"Error: {label}")
+                resource_name = names[1]
 
                 names = names[2:]
 
-            id = f"{type}.{name}"
-            if id not in parent.resources:
-                debug(f"add {id} into {parent.get_id()}")
-                parent.add_resource(Resource(keyword, type, name))
-            parent = parent.resources[id]
+            resource = Resource(label, type, resource_type, resource_name)
+
+            if resource.get_name() not in parent.resources:
+                debug(f"add {resource.get_name()} into {parent.get_name()}")
+                parent.add_resource(resource)
+            parent = parent.resources[resource.get_name()]
 
     return TerraformGraph(root.resources)
 
 
 def convert_to_digrams(terraform_graph: TerraformGraph):
     with Diagram("dist/terraform", show=False, graph_attr={"compound": "true"}):
-        convert_to_digrams_rec(terraform_graph.resources)
+        cache = {}
+        convert_to_digrams_rec(terraform_graph.resources, cache)
 
 
-def convert_to_digrams_rec(resources: dict[str, Resource]):
+def convert_to_digrams_rec(resources: dict[str, Resource], cache):
     for resource in resources.values():
-        if resource.keyword == "provider":
+        if resource.type == "provider":
             continue
-        if resource.keyword == "data":
+        if resource.type == "data":
             continue
-        if resource.keyword == "var":
+        if resource.type == "var":
             continue
 
-        if resource.keyword == "module":
-            with Cluster(resource.get_id()) as cluster:
-                convert_to_digrams_rec(resource.resources)
+        if resource.type == "module":
+            with Cluster(resource.get_name()) as cluster:
+                convert_to_digrams_rec(resource.resources, cache)
+                cache[resource.id] = cluster
         else:
             node = convert_to_node(resource)
+            cache[resource.id] = node
 
 
 def convert_to_node(resource: Resource):
-    return Node(resource.get_id())
+    return Node(resource.get_name())
